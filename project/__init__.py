e9a99f6528ec13be352776c5d8489e93caf9b8ae
IgorR <igorrozhnov95@gmail.com>
2023-01-23 19:45:38 +0300

    added schedule/APScheduler

--- project/__init__.py
+++ project/__init__.py
@@ -1,16 +1,32 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_apscheduler import APScheduler
 
 
 app=Flask(__name__)
+scheduler = APScheduler()
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://test:test@db/test'
+
 
 db=SQLAlchemy(app)
 migrate=Migrate(app, db)
 
-from . import views
+
+def start_scheduler():
+    from project.functions import add_in_database as scheduled_task
+    scheduler.init_app(app)
+    scheduler.start()
+    scheduler.add_job(id='scheduled_task', 
+                      func=scheduled_task, 
+                      trigger='interval', 
+                      seconds=10)
+
+
+from project import views
 
 if __name__ == "__main__":
+    start_scheduler()
     app.run()
